fbd68edcb49dc08d6fea6f4c0e34f406ed6ea82f
Teddygt02 <c14180127@john.petra.ac.id>
2020-04-16 19:07:25 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  item = [{
+    nama : 'buah kelengkeng',
+    harga : 10000
+  },{
+    nama : 'buah jeruk',
+    harga : 5000
+  }]
+
+
+  itemArr= [1,2,3,4];
+
+  show : boolean = true;
+  user ={
+    nama : 'along'
+  }
+
+  Pesan = '';
+  
+  KlikBut(){
+    this.Pesan = 'halo';
+  }
 }
